3ba66971cfc20731b7a7b3d6e6c6b0c244ce29cf
sp1dermanhackeryoutube <47869841+sp1dermanhackeryoutube@users.noreply.github.com>
2019-02-21 22:10:58 +0100

    spammer.py
    Ye, it's a pretty easy coded tool, it will open browsers if you answer the math wuestion incorrect. it will also play random youtube songs.

---
+++ Spammer.py
@@ -0,0 +1,1074 @@
+print(
+    "This is a quiz. Everytime you get an incorrect answer, you will be punished. You will be redirected to pages of porn etc. (If your PC is abput to break, please press ctrl+shift+esc and then right click on internet explorer and press stop activity")
+agree = input("Do you agree on the terms and conditions? Yes/No: ")
+agrees = "Yes"
+agreess = "yes"
+print("")
+if agree == agrees or agree == agreess:
+    print("OK let's start the quiz!")
+else:
+    print("You didnt accept...closing...") + quit()
+print("")
+number = input("What's 78+98?: ")
+numberr = "176"
+print("")
+if number == numberr:
+    print("Correct! You are very lucky") + quit()
+else:
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "https://www.youtube.com/watch?v=k85mRPqvMbE"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "https://www.fbi.gov/"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "https://www.fbi.gov/"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "https://www.youtube.com/watch?v=2BIWNLC_iT4&t=15s"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "https://www.fbi.gov/"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "https://www.fbi.gov/"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "https://www.fbi.gov/"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "https://www.fbi.gov/"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "https://www.fbi.gov/"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "https://www.youtube.com/watch?v=Y_IS_ccjkZE&t=0s"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+    url = "pornhub.com"
+    webbrowser.open_new_tab(url)
+
+    import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+import webbrowser
+
+url = "pornhub.com"
+webbrowser.open_new_tab(url)
+
+
+
